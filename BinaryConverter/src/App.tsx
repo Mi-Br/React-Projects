d83ec38a3037ae46f20894a6a49c1879635649fd
Michail <michailbredichin@gmail.com>
2023-04-22 12:43:47 +0300

    binary converter implemeneted

--- BinaryConverter/src/App.tsx
+++ BinaryConverter/src/App.tsx
@@ -10,6 +10,24 @@ function App() {
   });
 
 
+
+
+  const [error, setError] = useState(false)
+  const [result, setResult] = useState<number>()
+
+  type fieldInput = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+
+  function onInputChange({ target }: fieldInput) {
+    // console.log(target.value)
+    if (target.value.length > 0) {
+      setError(!(/^[01]+$/.test(target.value)))
+      if (!error) {
+        const base10Number = parseInt(target.value, 2);
+        setResult(base10Number)
+      }
+    }
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -19,9 +37,11 @@ function App() {
             <Grid display="grid" justifyItems={'center'} alignItems={'center'} sx={{ height: '100vh' }}>
               <Card sx={{ padding: '3rem' }}>
                 <CardContent>
-                  <Typography variant='h2'>Binary converter</Typography>
+                  <Typography variant='h3'>Binary converter</Typography>
                   <p>Use following calculator to convert binary digits to any other base</p>
-                  <TextField variant='outlined' fullWidth sx={{ marginTop: '2rem' }} label='binary number' error helperText='binary input only'></TextField>
+                  <TextField variant='outlined' fullWidth sx={{ marginTop: '2rem' }} label='binary number' error={error} helperText={error ? 'binary input only' : ''} onChange={e => onInputChange(e)}></TextField>
+                  <Typography variant='caption'>Result: </Typography>
+                  <Typography variant='overline'>{result}</Typography>
                 </CardContent>
               </Card>
             </Grid>
